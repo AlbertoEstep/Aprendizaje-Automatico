6f774ceeaf1ce1d2e218278066c9741fd017b18c
AlbertoEstep <albertoestep@gmail.com>
2020-02-25 11:55:14 +0100

    I add comments to the code

--- P0/p0.py
+++ P0/p0.py
@@ -36,9 +36,9 @@ for clase in clases:
     plt.scatter(x[index, 0], x[index, 1], c=colores[clase], label=nombres[clase])
 
 # Incluimos los nombres en los ejes, el título del gráfico y la leyenda, y la pintamos
-plt.xlabel('Petal length')
-plt.ylabel('Petal width')
-plt.title('Iris dataset classification')
+plt.xlabel('Longitud del pétalo')
+plt.ylabel('Ancho del pétalo')
+plt.title('Conjunto de datos flor iris')
 plt.legend()
 plt.show()
 
@@ -56,10 +56,17 @@ sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
 for train_index, test_index in sss.split(x, y):
     training_x, test_x = x[train_index], x[test_index]
     training_y, test_y = y[train_index], y[test_index]
-print(training_x)
-print(training_y)
-print(test_x)
-print(test_y)
+
+# Pintamos los datos de entrenamiento
+input("Pulsar \"Enter\" para visualizar los datos de entrenamiento de x")
+print("\nDatos de entrenamiento de x: \n" + str(training_x))
+input("\n\nPulsar \"Enter\" para visualizar los datos de entrenamiento de y")
+print("\nDatos de entrenamiento de y: \n" + str(training_y))
+# Pintamos los datos de test
+input("\n\nPulsar \"Enter\" para visualizar los datos de test de x")
+print("\nDatos de test de x: \n" + str(test_x))
+input("\n\nPulsar \"Enter\" para visualizar los datos de test de y")
+print("\nDatos de test de y: \n" + str(test_y))
 
 
 # ------------------------------------------------------------------------------
@@ -67,24 +74,31 @@ print(test_y)
 # ------------------------------------------------------------------------------
 
 # Obtener 100 valores equiespaciados entre 0 y 2π.
+input("\n\nPulsar \"Enter\" para mostrar los 100 valores equiespaciados entre 0 y 2π")
 z = np.linspace(0, 2*np.pi, 100)
 print(z)
 
 # Obtener el valor de sin(x), cos(x) y sin(x) + cos(x) para los 100 valores
 # anteriormente calculados.
+input("\n\nPulsar \"Enter\" para calcular el seno de los 100 valores equiespaciados entre 0 y 2π")
 sin_z = np.sin(z)
-print(sin_z)
+print("\n" + str(sin_z))
+input("\n\nPulsar \"Enter\" para calcular el coseno de los 100 valores equiespaciados entre 0 y 2π")
 cos_z = np.cos(z)
-print(cos_z)
+print("\n" + str(cos_z))
+input("\n\nPulsar \"Enter\" para calcular la suma seno + coseno de los 100 valores equiespaciados entre 0 y 2π")
 suma = sin_z + cos_z
-print(suma)
+print("\n" + str(suma))
+
 
 # Visualizar las tres curvas simultáneamente en el mismo plot (con líneas
 # discontinuas en negro, azul y rojo).
 
+# Solucion: con "color" indicamos el color, con "dashes" indicamos que las líneas
+# sean discontinuas y con "label" indicamos la etiqueta para la leyenda
+input("\n\nPulsar \"Enter\" para calcular visualizar simultaneamente las tres curvas")
 plt.plot(z, sin_z, color="black", dashes=[6, 2], label="seno")
 plt.plot(z, cos_z, color="blue", dashes=[6, 2], label="coseno")
 plt.plot(z, suma, color="red", dashes=[6, 2], label="seno + coseno")
-
 plt.legend(loc='lower left')
 plt.show()
